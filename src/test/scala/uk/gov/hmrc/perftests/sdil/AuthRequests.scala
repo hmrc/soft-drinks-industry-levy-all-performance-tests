cc844b69ea10389726c2216ed60b8d32e7135e35
Jayen Kerai <jayen.kerai@digital.hmrc.gov.uk>
2023-05-04 16:31:06 +0100

    DLS-7617: removed uneeded files and changed formatting

---
+++ src/test/scala/uk/gov/hmrc/perftests/sdil/AuthRequests.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.perftests.sdil
+
+import io.gatling.core.Predef._
+import io.gatling.core.check.CheckBuilder
+import io.gatling.core.check.regex.RegexCheckType
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+
+object AuthRequests extends BaseRequest {
+
+  val authWizUrl: String = baseUrlFor("auth-login-stub") + "/auth-login-stub/gg-sign-in"
+  val authSession: String = baseUrlFor("auth-login-stub") + "/auth-login-stub/session"
+  val bearerTokenPattern = """<td data-session-id="authToken" style="word-break: break-all">
+                             |            <code style="border: none">([^"]+)</code>""".stripMargin
+
+  def saveBearerTokenFromBody(): CheckBuilder[RegexCheckType, String, String] = regex(_ => bearerTokenPattern).saveAs("bearerToken")
+
+  lazy val navigateToAuth: HttpRequestBuilder =
+    http("Navigate to Auth wizard")
+      .get(authWizUrl)
+      .check(status.is(200))
+
+  lazy val navigateToAuthSession: HttpRequestBuilder =
+    http("Navigate to Auth Session page")
+      .get(authSession)
+      .check(status.is(200))
+      .check(saveBearerTokenFromBody())
+
+  def createAuthSession(utr: String = "0000000022"): HttpRequestBuilder =
+    http("Create Auth Session")
+      .post(authWizUrl)
+      .formParam("redirectionUrl", authSession)
+      .formParam("enrolment[0].name", "IR-CT")
+      .formParam("enrolment[0].taxIdentifier[0].name", "UTR")
+      .formParam("enrolment[0].taxIdentifier[0].value", utr)
+      .formParam("enrolment[0].state", "Activated")
+      .check(status.is(303))
+}
--- src/test/scala/uk/gov/hmrc/perftests/sdil/SDILSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/sdil/SDILSimulation.scala
@@ -16,39 +16,22 @@
 
 package uk.gov.hmrc.perftests.sdil
 
-import io.gatling.core.action.builder.ActionBuilder
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
 import uk.gov.hmrc.perftests.sdil.SDILRequests._
-import uk.gov.hmrc.perftests.sdil.AwesomeStubRequests._
+import uk.gov.hmrc.perftests.sdil.AuthRequests._
 
 class SDILSimulation extends PerformanceTestRunner {
 
-  setup("home-page", "Home Page") withRequests navigateToHomePage
-
-  setup("post-vat-return-period", "Post vat return period") withRequests postVatReturnPeriod
-
-  setup("get-turnover-page", "Get turnover page") withRequests getTurnoverPage
+  setup("sdil-returns-journey", "SDIL Returns journey").withRequests(
+    navigateToAuth,
+    createAuthSession(),
+    navigateToAuthSession,
+    getOwnBrandsPackagedAtOwnSitesPage,
+    postOwnBrandsPackagedAtOwnSitesPage,
+    getHowManyOwnBrandsPackagedAtOwnSitesPage,
+    postHowManyOwnBrandsPackagedAtOwnSitesPage
+  )
 
   runSimulation()
-
-  val useAwesomeStubs: Boolean =
-    readProperty("useAwesomeStubs", "false").toBoolean
-
-  def LoginTheUser(userId: String, eoriValue: String): List[ActionBuilder] =
-      List(
-        getLoginPage,
-        loginUser(userId),
-        updateUserRole(eoriValue),
-        postSuccessfulLogin
-      )
-
-  val ReturnsJourney: List[ActionBuilder] =
-    LoginTheUser("user1", "0000000022") ++
-      List[ActionBuilder](
-        getOwnBrandsPackagedAtOwnSitesPage,
-        postOwnBrandsPackagedAtOwnSitesPage,
-        getHowManyOwnBrandsPackagedAtOwnSitesPage,
-        postHowManyOwnBrandsPackagedAtOwnSitesPage
-      )
 
 }
